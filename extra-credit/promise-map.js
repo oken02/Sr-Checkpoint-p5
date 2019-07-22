40dcb464577da00204a07ecb18e4f5d69cba312c
ombaez <ombaez5689@gmail.com>
2019-07-22 14:48:50 -0300

    commitForUpdateApreviewVersion

--- extra-credit/promise-map.js
+++
@@ -1,9 +0,0 @@
-Promise.map = (collection, iterator) => {
-    return new Promise(function (resolve, reject) {
-        const arr =[] 
-        collection.map(element => arr.push(iterator(element)))
-
-        resolve(arr);
-
-    })
-};
